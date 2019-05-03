610c35f83cc66b021ec29c1188b9ed058dd4e0e4
abhilash <abhilash@ordant.com>
2019-05-03 13:04:36 +0530

    404 page

--- client/src/views/PageNotFound.vue
+++ client/src/views/PageNotFound.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>Page Not Found!!!</div>
+  <div class="error__page-404">
+    <b-jumbotron class="text-center mb-0" header="Oops" lead="404 PAGE NOT FOUND">
+      <p>
+        The page you are searching might have been removed
+        <br>or is temporarily unavailable
+      </p>
+
+      <router-link to="/">
+        <b-button variant="primary">Go To Home Page</b-button>
+      </router-link>
+    </b-jumbotron>
+  </div>
 </template>
 
 <script>
@@ -7,3 +18,8 @@ export default {
   name: "PageNotFound"
 };
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "../styles/views/error/404-page.scss";
+</style>
