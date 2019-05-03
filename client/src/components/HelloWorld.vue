25c26bc06c45da274521e3a333fd9931dad753a4
abhilash <abhilash@ordant.com>
2019-05-03 12:34:32 +0530

    scss setup

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,15 +1,16 @@
-<template></template>
+<template>
+
+</template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style scoped lang="scss">
 </style>
